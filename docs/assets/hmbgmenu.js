97e3e54a12ac2b6b755bbdf8661d7b6b24769665
krishnakanala <sendmailtokrish7@gmail.com>
2024-10-02 07:41:17 +0100

    Create hmbgmenu.js

---
+++ docs/assets/hmbgmenu.js
@@ -0,0 +1,9 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+        navLinks.style.flexDirection = 'column';
+    }
+});
